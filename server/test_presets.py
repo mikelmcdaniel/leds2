002302ca8e4f392e8ec423120380ae0a9d27f580
Mikel Mcdaniel <mikelmcdaniel@gmail.com>
2015-01-13 20:10:24 -0800

    Add very crude/rough profiling/testing for presets.

---
+++ server/test_presets.py
@@ -0,0 +1,27 @@
+import sys
+import cProfile
+
+import led_controller
+import presets
+import presets.all_presets
+
+def run_preset(preset, num_iterations=10**4):
+  leds = led_controller.FakeLeds(200)
+  seconds_per_frame = preset.seconds_per_frame
+  for _ in xrange(num_iterations):
+    preset.draw(leds.pixels, seconds_per_frame)
+  del leds
+
+def profile_presets():
+  global PRESET
+  for preset in presets.PRESETS:
+    PRESET = preset
+    print '***', preset.name
+    cProfile.run('run_preset(PRESET)')
+
+
+def main(argv):
+  profile_presets()
+
+if __name__ == '__main__':
+  main(sys.argv)
